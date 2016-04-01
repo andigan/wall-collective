822f6deb4eea285bb8cfe44592916c28008830a0
andigan <andrew.nease.code@gmail.com>
2016-04-01 10:26:04 -0400

    update .gitignore

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+//router.get('/', function(req, res) {
+//  res.render('three', { title: 'Express' });
+//});
+
+
+
+//router.get('/three', function (req, res){
+
+//    res.render('three', { title: 'three js'});
+
+
+//});
+
+
+module.exports = router;
